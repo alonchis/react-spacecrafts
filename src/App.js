6b2c6ba832919aadd39210407b71749e21885a5f
Alonso Arteaga <alonso.arteaga@wowinc.com>
2020-04-17 00:48:28 -0500

    add starry background, spinning voyager image, and brief tech summary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './voyager.png';
 import './App.css';
+
+class Probe extends React.Component {
+  render() {
+    return (
+        <div className="item">
+          <h1>Spacecraft In Space {this.props.name}</h1>
+          <p>Voyager 1 & 2</p>
+          <ul>
+            <li>Command computer Subsystem</li>
+            <li>Altitude and Articulation Control Subsystem</li>
+            <li>Uplink Communications via S-band</li>
+            <li>Radioisotope thermoelectric Generators (RTG)</li>
+          </ul>
+        </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Probe />
+          <div id="Stars"></div>
       </header>
     </div>
   );
